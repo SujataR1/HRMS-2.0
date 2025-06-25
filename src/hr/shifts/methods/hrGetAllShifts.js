6c0eaaa56331e77694fafb451c19e950417896bf
L-9 Transmogrify <transmogrify21@outlook.com>
2025-06-25 10:09:14 +0530

    Fixed spelling

--- src/hr/shifts/methods/hrGetAllShifts.js
+++ src/hr/shifts/methods/hrGetAllShifts.js
@@ -18,7 +18,7 @@ export async function hrGetAllShifts() {
 
 				fullShiftGraceInTimingInMinutes: true,
 				halfShiftGraceInTimingInMinutes: true,
-				fullShiftGraceOutTiminingInMinutes: true,
+				fullShiftGraceOutTimingInMinutes: true,
 				halfShiftGraceOutTimingInMinutes: true,
 
 				fullShiftEarlyPunchConsiderTimeInMinutes: true,
