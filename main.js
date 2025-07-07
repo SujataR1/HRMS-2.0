f4df54499f5b073dfc800f7a5e55d177d3fea2a0
L-9 Transmogrify <transmogrify21@outlook.com>
2025-07-07 11:42:36 +0530

    Added an endpoint to view employeeAttendance

--- main.js
+++ main.js
@@ -60,6 +60,7 @@ import hrVerify2FAAndLoginRoute from "./src/hr/secondary-authentication/routes/h
 import hrCreateAShiftRoute from "./src/hr/shifts/routes/hrCreateAShiftRoute.js";
 import hrGetAllShiftsRoute from "./src/hr/shifts/routes/hrGetAllShiftsRoute.js";
 import hrGetProfileRoute from "./src/hr/profile/routes/hrGetProfileRoute.js";
+import hrGetEmployeeAttendanceRoute from "./src/hr/attendance/routes/hrGetEmployeeAttendanceRoute.js";
 
 import employeeLoginRoute from "./src/employee/auth/routes/employeeLoginRoute.js";
 import employeeLogoutRoute from "./src/employee/auth/routes/employeeLogoutRoute.js";
@@ -181,6 +182,8 @@ await app.register(hrRequestAPasswordResetRoute);
 await app.register(hrResetPasswordRoute);
 await app.register(hrChangePasswordRoute);
 await app.register(hrGetProfileRoute);
+await app.register(hrGetEmployeeAttendanceRoute);
+
 await app.register(employeeLoginRoute);
 await app.register(employeeLogoutRoute);
 await app.register(employeeChangePasswordRoute);
