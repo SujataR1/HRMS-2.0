8fd061e2c65348a9443840bfb14376c801a0655b
L-9 Transmogrify <transmogrify21@outlook.com>
2025-07-14 11:37:36 +0530

    Added routes

--- src/hr/leave/methods/hrGetPendingLeaves.js
+++ src/hr/leave/methods/hrGetPendingLeaves.js
@@ -1,6 +1,6 @@
 import dayjs from "dayjs";
-import utc from "dayjs/plugin/utc";
-import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc.js";
+import timezone from "dayjs/plugin/timezone.js";
 import { PrismaClient } from "@prisma/client";
 import { hrVerifyJWT } from "../../hr-session-management/methods/hrSessionManagementMethods.js";
 
