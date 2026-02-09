3bf90af68034aed1daddb1b2e1204b6bc8e585d8
AnubhabDey-TransEV <anubhabdey@tgplin.com>
2026-02-09 14:58:59 +0530

    Removed the requirement that the employeeId has to be a UUID from the route to demote HRs by the admin.

--- src/admin/miscellaneous/schemas/adminDemoteHRSchema.js
+++ src/admin/miscellaneous/schemas/adminDemoteHRSchema.js
@@ -3,5 +3,4 @@ import { z } from "zod";
 export const adminDemoteHRSchema = z.object({
 	employeeId: z
 		.string({ required_error: "Employee ID is required" })
-		.uuid("Employee ID must be a valid UUID"),
 });
